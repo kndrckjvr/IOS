21a6d88aec13c74977f31c35c456285e129cfa20
KENDRICK ANDREW COSCA <kjcosca@MACLAB32.local>
2017-11-03 15:10:54 +0800

    Added Acivity 3

--- Activity3/ViewController.swift
+++ Activity3/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,   UITextFieldDelegate {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var reset: UIButton!
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    @IBAction func submitBtn(_ sender: Any) {
+        label.text = textField.text
+    }
+    @IBAction func responderBtn(_ sender: Any) {
+        textField.resignFirstResponder()
+    }
+    @IBAction func resetBtn(_ sender: Any) {
+        label.text = "Label"
+        textField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
