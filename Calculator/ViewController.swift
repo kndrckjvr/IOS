6720bc2105978dca14840aec6a68662ec5a69d36
KENDRICK ANDREW COSCA <kjcosca@MACLAB32.local>
2017-11-03 15:17:59 +0800

    Added Machine Problem 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTf: UITextField!
+    @IBOutlet weak var secondTf: UITextField!
+    @IBOutlet weak var resLabel: UILabel!
+    @IBAction func addBtn(_ sender: Any) {
+    
+        let n1 = Float(firstTf.text!)
+        let n2 = Float(secondTf.text!)
+        if(n1 == nil || n2 == nil) {
+            resLabel.text = "Error: Numbers Only!"
+            return
+        }
+        resLabel.text = "Answer: \(n1! + n2!)";
+    }
+    @IBAction func subBtn(_ sender: Any) {
+        let n1 = Float(firstTf.text!)
+        let n2 = Float(secondTf.text!)
+        if(n1 == nil || n2 == nil) {
+            resLabel.text = "Error: Numbers Only!"
+            return
+        }
+        resLabel.text = "Answer: \(n1! - n2!)";
+        
+    }
+    @IBAction func divBtn(_ sender: Any) {
+        let n1 = Float(firstTf.text!)
+        let n2 = Float(secondTf.text!)
+        if(n1 == nil || n2 == nil) {
+            resLabel.text = "Error: Numbers Only!"
+            return
+        }
+        resLabel.text = "Answer: \(n1! / n2!)";
+        if(n2 == 0) {
+            resLabel.text = "Error: Undefined"
+        }
+    }
+    @IBAction func mulBtn(_ sender: Any) {
+        let n1 = Float(firstTf.text!)
+        let n2 = Float(secondTf.text!)
+        
+        if(n1 == nil || n2 == nil) {
+            resLabel.text = "Error: Numbers Only!"
+            return
+        }
+        resLabel.text = "Answer: \(n1! * n2!)";
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
