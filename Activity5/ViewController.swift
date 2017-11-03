6f144d70a5d150e44d3bf76fa7c8bae387099060
KENDRICK ANDREW COSCA <kjcosca@MACLAB32.local>
2017-11-03 15:16:12 +0800

    Added Activity 5

--- Activity5/ViewController.swift
+++ Activity5/ViewController.swift
@@ -8,8 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    let items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    let items1 = ["+", "-", "*", "/"]
+    var num1, num2, op;
+    
+    @IBOutlet weak var lblRes: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return items.count
+        } else if component == 1 {
+            return items1.count
+        } else {
+            return items.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return items[row]
+        } else if component == 1 {
+            return items1[row]
+        } else {
+            return items[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            num1 = row
+        } else if component == 1 {
+            num2 = row
+        } else {
+            op = row
+        }
+        setResults(num1, num2, op)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +61,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
